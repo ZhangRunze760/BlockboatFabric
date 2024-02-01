05c696bc0da94fde73746b2b53f9be0fba084c1a
ZhangRunze760 <weibo1960@outlook.com>
2024-02-02 03:33:02 +0800

    更新至1.20.4。

--- src/main/java/fun/jsserver/blockboat/client/BlockboatFabricClient.java
+++ src/main/java/fun/jsserver/blockboat/client/BlockboatFabricClient.java
@@ -1,7 +1,6 @@
 package fun.jsserver.blockboat.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 
 public class BlockboatFabricClient implements ClientModInitializer {
     @Override
